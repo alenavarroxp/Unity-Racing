783388a1d093e7ccb72f546d8d9d563b2c16504e
alenavarroxp <alejandronavarro.software@gmail.com>
2025-05-06 13:12:45 +0200

    Update car stats directly and key & stats movement

--- Unity-Racing/Assets/Script/GarageManager.cs
+++ Unity-Racing/Assets/Script/GarageManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using Vuforia;
 using System.Collections;
+using TMPro;
 
 
 public class GarageManager : MonoBehaviour
@@ -13,8 +14,18 @@ public class GarageManager : MonoBehaviour
     private int currentIndex = 0; // 0 = car1, 1 = car2
 
     public Transform keyObject;
-    public Vector3 offsetCar1 = new Vector3(-0.45f, 0f, 0f);
-    public Vector3 offsetCar2 = new Vector3(0.45f, 0f, 0f);
+    public Transform statsObject;
+    public Vector3 offsetCar1 = new Vector3(-0.45f, 1.484054f, -0.4393685f);
+    public Vector3 offsetCar2 = new Vector3(0.45f, 1.484054f, -0.4393685f);
+    public Vector3 offsetStats1 = new Vector3(-0.45f, 7.412627f, 0.2363281f);
+    public Vector3 offsetStats2 = new Vector3(0.45f, 7.412627f, 0.2363281f);
+
+    public TextMeshPro carNameText;
+    public TextMeshPro massText;
+    public TextMeshPro maxSpeedText;
+    public TextMeshPro reverseSpeedText;
+    public TextMeshPro turboText;
+
     public float moveDuration = 0.5f;
 
     private void Start()
@@ -54,9 +65,12 @@ public class GarageManager : MonoBehaviour
         car1.transform.localRotation = Quaternion.Euler(0, 0, 0);
         car2.transform.localRotation = Quaternion.Euler(0, 0, 0);
 
-        Vector3 targetLocalPos = index == 0 ? offsetCar1 : offsetCar2;
+        Vector3 targetKeyLocalPos = index == 0 ? offsetCar1 : offsetCar2;
+        Vector3 targetStatsLocalPos = index == 0 ? offsetStats1 : offsetStats2;
         StopAllCoroutines();
-        StartCoroutine(MoveKeySmoothly(targetLocalPos));
+        StartCoroutine(MoveKeySmoothly(targetKeyLocalPos));
+        StartCoroutine(MoveStatsSmoothly(targetStatsLocalPos));
+        UpdateCarStatsUI();
     }
 
     public void NextCar()
@@ -73,17 +87,52 @@ public class GarageManager : MonoBehaviour
 
     private IEnumerator MoveKeySmoothly(Vector3 targetLocalPos)
     {
-        Vector3 startLocalPos = keyObject.localPosition;
+        Vector3 startKeyLocalPos = keyObject.localPosition;
         float elapsed = 0f;
 
         while (elapsed < moveDuration)
         {
-            keyObject.localPosition = Vector3.Lerp(startLocalPos, targetLocalPos, elapsed / moveDuration);
+            keyObject.localPosition = Vector3.Lerp(startKeyLocalPos, targetLocalPos, elapsed / moveDuration);
             elapsed += Time.deltaTime;
             yield return null;
         }
 
         keyObject.localPosition = targetLocalPos;
+    }
+
+    private IEnumerator MoveStatsSmoothly(Vector3 targetLocalPos)
+    {
+        Vector3 startStatsLocalPos = statsObject.localPosition;
+        float elapsed = 0f;
+
+        while (elapsed < moveDuration)
+        {
+            statsObject.localPosition = Vector3.Lerp(startStatsLocalPos, targetLocalPos, elapsed / moveDuration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        statsObject.localPosition = targetLocalPos;
+    }
+
+    private void UpdateCarStatsUI()
+    {
+        if (currentIndex == 0)
+        {
+            carNameText.text = "ARCADE - Free Racing Car";
+            massText.text = "1000kg";
+            maxSpeedText.text = "100kmh";
+            reverseSpeedText.text = "35kmh";
+            turboText.text = "Yes";
+        }
+        else
+        {
+            carNameText.text = "PROMETEO";
+            massText.text = "900kg";
+            maxSpeedText.text = "120kmh";
+            reverseSpeedText.text = "50kmh";
+            turboText.text = "No";
+        }
     }
 
 }
