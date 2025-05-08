725b15d6f9c73519f9fd2fa3d04f36b8092e41ec
alenavarroxp <alejandronavarro.software@gmail.com>
2025-05-09 00:37:40 +0200

    update vuforia package and fix errors

--- Unity-Racing/Assets/Scripts/StartController.cs
+++ Unity-Racing/Assets/Scripts/StartController.cs
@@ -5,6 +5,7 @@ public class StartController : MonoBehaviour
     public GameObject objectManager;
     public GameObject lampostManager;
     public GameObject fuelBar;
+    public GameObject turboBar;
     public GameObject objectBar;
     public GameObject buttons;
     public GameObject speedText;
@@ -14,6 +15,7 @@ public class StartController : MonoBehaviour
         if (objectManager != null) objectManager.SetActive(true);
         if (lampostManager != null) lampostManager.SetActive(true);
         if (fuelBar != null) fuelBar.SetActive(true);
+        if (turboBar != null) turboBar.SetActive(true);
         if (objectBar != null) objectBar.SetActive(true);
         if (buttons != null) buttons.SetActive(true);
         if (speedText != null) speedText.SetActive(true);
